56ed451612b662f0cb3d70bb61df1e4af217cbb0
Nitin Baluni <nitin.baluni@mail.vinove.com>
2023-12-17 15:33:37 +0530

    Update .gitignore

--- staging/wp-content/themes/workstatus/common/available-device.php
+++
@@ -1,7 +0,0 @@
-<div class="av-device"><span>Available for:</span>
-  <i class="icon1 wslazy"></i>
-  <i class="icon2 wslazy"></i>
-  <i class="icon3 wslazy"></i>
-  <i class="icon4 wslazy"></i>
-  <i class="icon5 wslazy"></i>
-</div>
