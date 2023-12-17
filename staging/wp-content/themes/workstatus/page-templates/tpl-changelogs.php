4066524be8fe6d671ebf386e87e1509c616a92ef
Nitin Baluni <nitin.baluni@mail.vinove.com>
2023-12-17 16:35:59 +0530

    updated changes log

--- staging/wp-content/themes/workstatus/page-templates/tpl-changelogs.php
+++ staging/wp-content/themes/workstatus/page-templates/tpl-changelogs.php
@@ -47,7 +47,7 @@ if( isset( $data->code ) && ($data->code == 200) ) : ?>
         <?php  
         $index = 0;
         foreach( $data->data as $row ) : 
-        $index++;    
+        $index++;
         //echo '<pre>'; print_r( $row ); die; ?>    
             <tr id="tr-row-<?php echo $index; ?>" class="row1">
                 <td class="first"><h5><?php echo $row->phase ?></h5>
@@ -66,13 +66,15 @@ if( isset( $data->code ) && ($data->code == 200) ) : ?>
                     ?>
 
                     <div id="showallnotes" class="allnotes" style="margin-top:15px;">
-                    <div class="full-content" style="display:none">
-                        <?php echo $row->release_note; ?>
-                        <a href="javascript:void(0)" class="note-link hide-fcontent" data-row="<?php echo $index; ?>">Hide Release Notes</a>
-                    </div>
+                    <div class="full-content" 
+                    id="node-log-<?php echo $index; ?>"
+                    data-phase="<?php echo $row->phase ?>"
+                    data-platform="<?php echo isset( $row->version_detail ) ? $row->version_detail->{'Web Platform'}->version : ''; ?>"
+                    data-publish="<?php echo date('jS M\'Y', strtotime($row->date)); ?>"
+                    style="display:none"><?php echo $row->release_note; ?></div>
                     <div class="full-content-excerpt" style="display:block;">
                     <?php 
-                    echo substr($row->release_note, 0, 100).'... <a href="javascript:void(0)" class="note-link hide-excerpt" data-row="'.$index.'">Show Release Notes</a>';
+                    echo substr($row->release_note, 0, 100).'... <a href="javascript:void(0)" onclick="popReleaseNote('.$index.')" class="note-link hide-excerpt" data-row="'.$index.'">Show Release Notes</a>';
                     ?>                                        
                     </div>
                     </div>
@@ -152,31 +154,80 @@ if( isset( $data->code ) && ($data->code == 200) ) : ?>
     </div>
 </div>
 <?php endif; ?>
-<section></section>
+<div class="banner-form chlog-popup">
+  <div id="chlog-popup" class="popup-wrapper" style="display:none;">
+    <div class="popWrap">
+      <div class="popup-content">
+      <!-- <span class="closeicon" onclick="_hideCLpopup();"></span> -->
+        <div class="head-top">
+          <h3>Release Notes </h3>
+          <div class="phase-sec">
+            <div class="flex-2">
+                <h4 id="cl-phase">Phase #7.3</h4>
+                <p id="cl-date">Published on 4th Dec'2023</p>
+            </div>
+            <div class="flex-2" id="cl-platform">
+                <h4 id="cl-platform">Web Platform</h4>
+                <p id="cl-version">Version 3.1</p>
+            </div>
+          </div>
+        </div>
+        <div class="change-content">
+          <div class="content-wrap" id="cl-content"></div>
+          <div class="ctasec">
+          <a href="javascript:void(0);" class="primary_btn1" onclick="_hideCLpopup();">Hide Release Notes</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <script type="text/javascript">
- document.addEventListener('click', function (event){
-    var hider   = event.target;
-    if(event.target.matches('a.hide-excerpt')){
+ // document.addEventListener('click', function (event){
+ //    var hider   = event.target;
+ //    if(event.target.matches('a.hide-excerpt')){
 
-        /*var fullContent = document.querySelectorAll(".full-content");
-        for( var i = 0; i < fullContent.length; ++i ){
-            fullContent[i].classList.add('hider');
-        }*/
-        var tfull   = "#tr-row-"+hider.getAttribute('data-row')+" div.full-content";
-        var texpr   = "#tr-row-"+hider.getAttribute('data-row')+" div.full-content-excerpt";
-        document.querySelector(texpr).style.display="none";
-        document.querySelector(tfull).style.display="block";
-    }
-    if(event.target.matches('a.hide-fcontent')){
-        /*var fullContent = document.querySelectorAll(".full-content-excerpt");
-        for( var i = 0; i < fullContent.length; ++i ){
-            fullContent[i].classList.add('hider');
-        }*/
-        var tfull   = "#tr-row-"+hider.getAttribute('data-row')+" div.full-content";
-        var texpr   = "#tr-row-"+hider.getAttribute('data-row')+" div.full-content-excerpt";
-        document.querySelector(texpr).style.display="block";
-        document.querySelector(tfull).style.display="none";
-    }
- });
+ //        /*var fullContent = document.querySelectorAll(".full-content");
+ //        for( var i = 0; i < fullContent.length; ++i ){
+ //            fullContent[i].classList.add('hider');
+ //        }*/
+ //        var tfull   = "#tr-row-"+hider.getAttribute('data-row')+" div.full-content";
+ //        var texpr   = "#tr-row-"+hider.getAttribute('data-row')+" div.full-content-excerpt";
+ //        document.querySelector(texpr).style.display="none";
+ //        document.querySelector(tfull).style.display="block";
+ //    }
+ //    if(event.target.matches('a.hide-fcontent')){
+ //        /*var fullContent = document.querySelectorAll(".full-content-excerpt");
+ //        for( var i = 0; i < fullContent.length; ++i ){
+ //            fullContent[i].classList.add('hider');
+ //        }*/
+ //        var tfull   = "#tr-row-"+hider.getAttribute('data-row')+" div.full-content";
+ //        var texpr   = "#tr-row-"+hider.getAttribute('data-row')+" div.full-content-excerpt";
+ //        document.querySelector(texpr).style.display="block";
+ //        document.querySelector(tfull).style.display="none";
+ //    }
+ // });
+
+function popReleaseNote( indx ){
+    var thisDom     = document.getElementById('node-log-'+indx);
+    var clContent   = document.getElementById('cl-content');
+
+    let pubDate     = thisDom.getAttribute('data-publish');
+    let pubPhase    = thisDom.getAttribute('data-phase');
+    let pubVersion  = thisDom.getAttribute('data-platform');
+
+    clContent.innerHTML = thisDom.innerHTML;
+    document.getElementById('cl-phase').innerHTML = pubPhase;
+    document.getElementById('cl-date').innerHTML = pubDate;
+    document.getElementById('cl-version').innerHTML = pubVersion;
+    
+
+    document.getElementById('chlog-popup').style.display = 'block';
+    //console.log( thisDom );
+}
+
+function _hideCLpopup(){
+    document.getElementById('chlog-popup').style.display = 'none';
+}
 </script>                   
 <?php get_footer(); ?>
