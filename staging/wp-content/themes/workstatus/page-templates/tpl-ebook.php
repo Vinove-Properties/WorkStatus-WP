898c084182a2f6fad7ef49592aaa2eeea2c887fb
nandani <nandani.sinha@mail.vinove.com>
2024-02-05 15:31:59 +0530

    Wp Ebbok

--- staging/wp-content/themes/workstatus/page-templates/tpl-ebook.php
+++ staging/wp-content/themes/workstatus/page-templates/tpl-ebook.php
@@ -7,21 +7,52 @@ get_header();
 global $ws_ctas, $RegLink, $LogLink;
 ?>
 <main class="site__content">
-<section class="overview">
-<div class="container">
-  <div class="top-section">
-    <?php 
+
+<section class="hero-banner inner-banner-bg sd-banner post-animation">
+        <div class="bannerwrap">
+          <div class="container">
+            <div class="inner-wrap">
+            <?php 
     while(have_posts()) : the_post(); 
       the_content();
     endwhile;
     ?>
-  </div>
+            </div>
+          </div>
+        </div>
+      </section>
+<section class="overview">
+<div class="container">
   <div class="dis-flex">
     <div class="colleft">
       <?php the_field('overview-text'); ?>
     </div>
     <div class="colRit" id="ebook-pdf" data-edoc="<?php the_field('epdf'); ?>">
         <?php require_once get_template_directory().'/common/ebook-form.php'; ?>
+    </div>
+  </div>
+</section>
+
+<section class="top-plateform p50 post-animation ebook">
+  <div class="container">
+    <div class="flex_row">
+      <div class="column-left">
+        <picture>
+          <source type="image/webp" srcset="<?php echo SITE_URL; ?>/assests/images/ebook.png">
+          <source type="image/png" srcset="<?php echo SITE_URL; ?>/assests/images/ebook.png">
+          <img loading="lazy" src="<?php echo SITE_URL; ?>/assests/images/ebook.png"
+            alt="Ebook" width="602" height="457">
+        </picture>
+      </div>
+      <div class="column-right">
+        <h2>Lorem ipusm sit</h2>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </li>
+        </ul>
+      </div>
     </div>
   </div>
 </section>
