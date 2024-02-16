82242a08eac60bd6b238ee2b0b92f12674873adf
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-02-16 18:21:29 +0530

    home page section update

--- staging/wp-content/themes/workstatus/page-templates/tpl-home-v2.php
+++ staging/wp-content/themes/workstatus/page-templates/tpl-home-v2.php
@@ -178,7 +178,7 @@ if( $mServicesRows ){
           <img class="show" loading="lazy" src="'.$row['icon']['url'].'" alt="Icon" width="25" height="25">
           <img class="hide" loading="lazy" src="'.$row['hicon']['url'].'" alt="Icon" width="25" height="25">
           </span>'.$row['title'].'</a>
-          </li>'  
+          </li>';
           }
           ?>
         </ul>
