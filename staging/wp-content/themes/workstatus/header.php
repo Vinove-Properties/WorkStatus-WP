c85d2f7ee36a9de1a3f420ab63986ba04c97aa4d
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-03-12 18:04:27 +0530

    update core web vital

--- staging/wp-content/themes/workstatus/header.php
+++ staging/wp-content/themes/workstatus/header.php
@@ -23,8 +23,9 @@ global $ws_ctas, $RegLink, $LogLink;
     .wslazy{background-image:none !important; background-color:#F1F1FA !important; background:none !important;}
     .wsbg-cover{background-size:contain; background-repeat:no-repeat; background-position:center;} 
     
-    .inner-banner .videoSc .srp-1 .videoPoster.wsbg-img{background-image:none;}
-
+    .inner-banner .videoSc .srp-1 .videoPoster.wsbg-img,
+    .inner-banner .videoSc .srp-30 .videoPoster.wsbg-img{background-image:none;}
+    
     #ws-apimsg.success{margin-top: 10px; display:inline-block; color:#007600;}
     #ws-apimsg.error{margin-top: 10px; display:inline-block; color:#ed1010;}
     .useful-resources-blog-section .thumb img{height:200px!important;}
