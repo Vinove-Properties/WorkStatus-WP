4aaa524a6ca6eabbb6264d56e4428ed468a2fae5
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-03-22 16:49:40 +0530

    custimozation page update

--- staging/wp-content/themes/workstatus/header.php
+++ staging/wp-content/themes/workstatus/header.php
@@ -82,6 +82,9 @@ global $ws_ctas, $RegLink, $LogLink;
 
   body.page-id-7306 .actionBtn.ws-journey div:last-child,
   body.page-id-6785 .actionBtn.ws-journey div:last-child{display: block;}
+  
+  body.page-id-7306 #ws-comprehensive_dashboard_section .btnSc,
+  body.page-id-7306 #ws-boost_profitability .btnSc{display: none;}
 
 
 /*Diwali Banner Css Start From Here*/
