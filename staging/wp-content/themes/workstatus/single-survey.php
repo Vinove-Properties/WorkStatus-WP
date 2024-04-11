fff3a7ad133e926265f983e9978d6f6ac5a21956
nandani <nandani.sinha@mail.vinove.com>
2024-04-11 13:28:48 +0530

    Survey Basic structure

--- staging/wp-content/themes/workstatus/single-survey.php
+++ staging/wp-content/themes/workstatus/single-survey.php
@@ -1,115 +1,110 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-  </head>
-  <body>
-    <section class="survey-sec">
-      <div class="container">
-        <div class="logo">
-          <a href="<?php echo site_url(); ?>/" title="Workstatus">
-            <picture>
-              <img src="<?php echo get_template_directory_uri(); ?>/assests/images/logo.svg" alt="Workstatus" width="186" height="25"> 
-            </picture>
-          </a>
-        </div>
-
-        <div class="survey-box">
-        <h1>Here are a few questions to help us understand your experience better:</h1>
-        <form action="#" class="surform" method="POST">
-          <div class="question">
-            <h3>What initially attracted you to try out Workstatus?</h3>
-            <div class="form-field">
-            <input type="radio" id="color1" name="color" value="red">
-            <label for="color1">Positive reviews from other users</label>
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <?php wp_head(); ?>
+   </head>
+   <body>
+      <section class="survey-sec">
+         <div class="container">
+            <div class="logo">
+               <a href="<?php echo site_url(); ?>/" title="Workstatus">
+                  <picture>
+                     <img src="<?php echo get_template_directory_uri(); ?>/assests/images/logo.svg" alt="Workstatus" width="186" height="25"> 
+                  </picture>
+               </a>
             </div>
-            <div class="form-field">
-            <input type="radio" id="color2" name="color" value="red">
-            <label for="color2">Positive reviews from other users</label>
+            <div class="survey-box">
+               <h1>Here are a few questions to help us understand your experience better:</h1>
+               <form action="#" class="surform" method="POST">
+                  <div class="question">
+                     <h3>What initially attracted you to try out Workstatus?</h3>
+                     <div class="form-field">
+                        <input type="radio" id="color1" name="color" value="red">
+                        <label for="color1">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color2" name="color" value="red">
+                        <label for="color2">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color3" name="color" value="red">
+                        <label for="color3">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color4" name="color" value="red">
+                        <label for="color4">Positive reviews from other users</label>
+                     </div>
+                  </div>
+                  <div class="question">
+                     <h3>What initially attracted you to try out Workstatus?</h3>
+                     <div class="form-field">
+                        <input type="radio" id="color5" name="color" value="red">
+                        <label for="color5">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color6" name="color" value="red">
+                        <label for="color6">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color7" name="color" value="red">
+                        <label for="color7">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color8" name="color" value="red">
+                        <label for="color8">Positive reviews from other users</label>
+                     </div>
+                  </div>
+                  <div class="question">
+                     <h3>What initially attracted you to try out Workstatus?</h3>
+                     <div class="form-field">
+                        <input type="radio" id="color9" name="color" value="red">
+                        <label for="color9">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color10" name="color" value="red">
+                        <label for="color10">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color11" name="color" value="red">
+                        <label for="color11">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color12" name="color" value="red">
+                        <label for="color12">Positive reviews from other users</label>
+                     </div>
+                  </div>
+                  <div class="question">
+                     <h3>What initially attracted you to try out Workstatus?</h3>
+                     <div class="form-field">
+                        <input type="radio" id="color13" name="color" value="red">
+                        <label for="color13">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color14" name="color" value="red">
+                        <label for="color14">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color15" name="color" value="red">
+                        <label for="color15">Positive reviews from other users</label>
+                     </div>
+                     <div class="form-field">
+                        <input type="radio" id="color16" name="color" value="red">
+                        <label for="color16">Positive reviews from other users</label>
+                     </div>
+                  </div>
+                  <div class="conclusion">
+                     <h3>Special Offer</h3>
+                     <ul>
+                        <li>As a token of appreciation for your valuable feedback, we'd like to offer you a <strong>30%</strong> discount on our annual plan. Simply use the coupon code <strong>WS30March</strong> at checkout to redeem your discount.</li>
+                        <li>Thank you for participating in our survey! Your feedback helps us tailor our product to better meet your needs.</li>
+                     </ul>
+                  </div>
+                  <button type="submit">Submit My Feedback</button>
+               </form>
             </div>
-            <div class="form-field">
-            <input type="radio" id="color3" name="color" value="red">
-            <label for="color3">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color4" name="color" value="red">
-            <label for="color4">Positive reviews from other users</label>
-            </div>
-
-          </div>
-          <div class="question">
-            <h3>What initially attracted you to try out Workstatus?</h3>
-            <div class="form-field">
-            <input type="radio" id="color5" name="color" value="red">
-            <label for="color5">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color6" name="color" value="red">
-            <label for="color6">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color7" name="color" value="red">
-            <label for="color7">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color8" name="color" value="red">
-            <label for="color8">Positive reviews from other users</label>
-            </div>
-            
-          </div>
-          <div class="question">
-            <h3>What initially attracted you to try out Workstatus?</h3>
-            <div class="form-field">
-            <input type="radio" id="color9" name="color" value="red">
-            <label for="color9">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color10" name="color" value="red">
-            <label for="color10">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color11" name="color" value="red">
-            <label for="color11">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color12" name="color" value="red">
-            <label for="color12">Positive reviews from other users</label>
-            </div>
-            
-          </div>
-          <div class="question">
-            <h3>What initially attracted you to try out Workstatus?</h3>
-            <div class="form-field">
-            <input type="radio" id="color13" name="color" value="red">
-            <label for="color13">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color14" name="color" value="red">
-            <label for="color14">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color15" name="color" value="red">
-            <label for="color15">Positive reviews from other users</label>
-            </div>
-            <div class="form-field">
-            <input type="radio" id="color16" name="color" value="red">
-            <label for="color16">Positive reviews from other users</label>
-            </div>
-            
-          </div>
-          <div class="conclusion">
-            <h3>Special Offer</h3>
-            <ul>
-                <li>As a token of appreciation for your valuable feedback, we'd like to offer you a <strong>30%</strong> discount on our annual plan. Simply use the coupon code <strong>WS30March</strong> at checkout to redeem your discount.</li>
-<li>Thank you for participating in our survey! Your feedback helps us tailor our product to better meet your needs.</li>
-            </ul>
-          </div>
-          <button type="submit">Submit My Feedback</button>
-        </form>
-     </div>
-      </div>
-    </section>
-  </body>
+         </div>
+      </section>
+   </body>
 </html>
