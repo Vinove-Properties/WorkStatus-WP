b07a03243abcfaa0a04311c57ad3bb44cc2dede6
nandani <nandani.sinha@mail.vinove.com>
2024-04-15 10:28:00 +0530

    Survey TEXTAREA added

--- staging/wp-content/themes/workstatus/single-survey.php
+++ staging/wp-content/themes/workstatus/single-survey.php
@@ -1,27 +1,27 @@
 <?php 
-$postRequest = _hanldeSurveyRequest( $_POST );
-//echo decryptEmailData($_GET['uid']); die;
-?>
+  $postRequest = _hanldeSurveyRequest( $_POST );
+  //echo decryptEmailData($_GET['uid']); die;
+  ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-   <style type="text/css">
-   span.error{color: red; display: none;}   
-   .question.err span.error{display: block;}   
-   </style>
+    <style type="text/css">
+      span.error{color: red; display: none;}   
+      .question.err span.error{display: block;}   
+    </style>
   </head>
-  <body>   
-   <?php if( $postRequest === true ) : ?>
+  <body>
+    <?php if( $postRequest === true ) : ?>
     <div id="survey-thank" class="survey-thank">
       <div id="surveypop" class="popup-wrapper " style="display: block;">
         <div class="popWrap">
           <div class="popup-content">
             <span class="closeicon" onclick="_closeThanks('survey-thank');"></span>
             <picture>
-            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/dev-images/thank-img.svg" width="127" height="136" alt="Workstatus">
+              <img loading="lazy" src="<?php bloginfo('template_url'); ?>/dev-images/thank-img.svg" width="127" height="136" alt="Workstatus">
             </picture>
             <h2>Thank You</h2>
             <p>Thank you for participating in our survey! Your feedback helps us tailor our product to better meet your needs.</p>
@@ -29,8 +29,7 @@ $postRequest = _hanldeSurveyRequest( $_POST );
         </div>
       </div>
     </div>
-   <?php endif; ?>
-
+    <?php endif; ?>
     <section class="survey-sec">
       <div class="container">
         <div class="logo">
@@ -47,50 +46,48 @@ $postRequest = _hanldeSurveyRequest( $_POST );
           <h1><?php echo get_field('sy-title'); ?></h1>
           <form action="<?php echo get_permalink(); ?>" class="surform" method="POST" onsubmit="return _handle_survey(this);">
             <?php 
-            if( $ws_survey ){
-             $q = 0;
-             foreach( $ws_survey as $que ){ $q++;
-                $qu   = 'que-'.$q;
-                $ans  = 'ans-'.$q;
-                echo '<div id="ans-'.$q.'" class="question"><h3>'.$que['question'].'</h3>';
-                echo '<input type="hidden" name="'.$qu.'" value="'.$que['question'].'">';
-                if( $que['answears'] ){
-                   $op = 0;
-                   foreach( $que['answears'] as $opt){ $op++;
-                      echo '<div class="form-field">
-                      <input type="radio" id="'.$qu.'-'.$op.'" name="'.$ans.'" value="'.$opt['options'].'">
-                      <label for="'.$qu.'-'.$op.'">'.$opt['options'].'</label>
-                      </div>';
-                   }
-                }
-             echo '<span class="error">Please answer this question.</span>';
-             echo '</div>';
-             }
-            }
-            ?>
+              if( $ws_survey ){
+               $q = 0;
+               foreach( $ws_survey as $que ){ $q++;
+                  $qu   = 'que-'.$q;
+                  $ans  = 'ans-'.$q;
+                  echo '<div id="ans-'.$q.'" class="question"><h3>'.$que['question'].'</h3>';
+                  echo '<input type="hidden" name="'.$qu.'" value="'.$que['question'].'">';
+                  if( $que['answears'] ){
+                     $op = 0;
+                     foreach( $que['answears'] as $opt){ $op++;
+                        echo '<div class="form-field">
+                        <input type="radio" id="'.$qu.'-'.$op.'" name="'.$ans.'" value="'.$opt['options'].'">
+                        <label for="'.$qu.'-'.$op.'">'.$opt['options'].'</label>
+                        </div>';
+                     }
+                  }
+               echo '<span class="error">Please answer this question.</span>';
+               echo '</div>';
+               }
+              }
+              ?>
             <div class="question">
-              <div class="form-field">
-              <input type="checkbox" name="other" id="other-ck"><label for="other-ck">Other</label>
-              </div>
-              <div class="form-field">  
-                <textarea name="other-text"></textarea>
+              <div class="form-field checkfield">
+                <input type="checkbox" id="myCheck" />
+                Other
+                <div class="check-area" id="chkarea">
+                  <textarea name="other-text" class="textbox"></textarea>
+                </div>
               </div>
             </div>
-            
             <input type="hidden" name="post_id" value="<?php echo get_the_ID(); ?>">
             <?php 
-            $csrfNonce = wp_create_nonce( 'post_survey-'.get_the_ID() ); 
-            echo '<input type="hidden" name="su-nonce" value="'.$csrfNonce.'">';
-            ?>
+              $csrfNonce = wp_create_nonce( 'post_survey-'.get_the_ID() ); 
+              echo '<input type="hidden" name="su-nonce" value="'.$csrfNonce.'">';
+              ?>
             <input type="hidden" name="email_addr" 
-            value="<?php echo (isset($_GET['uid']) && !empty($_GET['uid'])) ? $_GET['uid'] : ''; ?>">
+              value="<?php echo (isset($_GET['uid']) && !empty($_GET['uid'])) ? $_GET['uid'] : ''; ?>">
             <button type="submit">Submit My Feedback</button>
-            
             <div class="conclusion">
-            <?php echo get_field('ws-offers'); ?>
+              <?php echo get_field('ws-offers'); ?>
             </div>
-
-            </form>
+          </form>
         </div>
       </div>
     </section>
