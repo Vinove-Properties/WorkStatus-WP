5cd4a665c904fdd40c85cc2150f8c9883b6c9e4f
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-05-06 14:48:37 +0530

    special offer count down

--- l/wp-content/themes/ws-landing/footer.php
+++ l/wp-content/themes/ws-landing/footer.php
@@ -13,5 +13,31 @@ if(!is_page_template([
 <?php //endif; ?> 
 -->
 <?php wp_footer(); ?>
+<script type="text/javascript">
+function startCountdown(duration, min, sec) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        
+        min.textContent = minutes;
+        sec.textContent = seconds;
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var cnMin = document.getElementById("of-min");
+    var cnSec = document.getElementById("of-sec");
+    var duration = 45 * 60; // 45 minutes in seconds
+
+    startCountdown(duration, cnMin, cnSec);
+};	
+</script>
 </body>
 </html>
