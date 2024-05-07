c3a1b522c8bbc75f82faecb47b74c2b5a0597311
nandani <nandani.sinha@mail.vinove.com>
2024-05-07 15:14:24 +0530

    WS Section Added

--- staging/wp-content/themes/workstatus/common/workstatus-journey-straight.php
+++ staging/wp-content/themes/workstatus/common/workstatus-journey-straight.php
@@ -1,25 +1,36 @@
 <?php global $ws_ctas, $RegLink, $LogLink; ?>
-<section class="start-joureny journey-bg text_center whitetxt">
-<div class="shape"></div>
-<div class="trianglefeaturedetail-right"></div>
-<div class="container">
-   <div class="top-section">
-      <h6><span class="bg-white">Don’t Wait</span></h6>
-      <h2>Start your Workstatus journey today!</h2>
-      <p>Get detailed and clean activity reports of your team.</p>
-   </div>
-   <div class="actionBtn center white ws-journey">
-      <div>
-         <a href="javascript:void(0)" onclick="call_demows();" class="primary_btn1 btnbg2">Book A Demo</a>
-         <small class="max-width-250">Get queries answered from experts</small>
+<section class="works-journey">
+  <div class="container">
+    <div class="ws-flex">
+      <div class="jour-col">
+        <div class="top-section">
+          <h6><span class="bg-white">Don’t Wait</span></h6>
+          <h2>Start your Workstatus journey today!</h2>
+          <p>Get detailed and clean activity reports of your team.</p>
+        </div>
+        <div class="actionBtn">
+          <div><a data-href="<?php echo $RegLink; ?>" href="javascript:void(0);" class="primary_btn1"  onclick="return get_ws_signupform(this);">Start Free Trial</a>
+            <small>No credit card required</small>
+          </div>
+          <?php if( ($ws_ctas !== false) && ($ws_ctas['show_this'] === true) ){ ?>
+          <div><a href="javascript:void(0)" onclick="call_demows();" class="primary_btn2 formbtn">Book A Demo</a>
+            <small>Get queries answered from experts</small>
+          </div>
+          <?php } ?>
+        </div>
       </div>
-      <?php if( ($ws_ctas !== false) && ($ws_ctas['show_this'] === true) ){ ?>
-      <div>
-         <a data-href="<?php echo $RegLink; ?>" href="javascript:void(0);" class="primary_btn2 btnwhite" 
-         onclick="return get_ws_signupform(this);"><?php echo $ws_ctas['cta_text']; ?></a>
-         <small class="max-width-184">No credit card required</small>
+      <span class="devide">OR</span>
+      <div class="jour-col">
+        <div class="top-section">
+          <h6><span class="bg-white">CUSTOMIZE YOUR WORKFLOW</span></h6>
+          <h2>Streamline with Smart Customization</h2>
+          <p>Integrating seamlessly with your operations.</p>
+        </div>
+        <div class="actionBtn">
+          <div><a href="https://calendly.com/workstatus/sales" href="javascript:void(0);" class="primary_btn1">Talk to Sales</a>
+          </div>
+        </div>
       </div>
-      <?php } ?>
-   </div>
-</div>
+    </div>
+  </div>
 </section>
