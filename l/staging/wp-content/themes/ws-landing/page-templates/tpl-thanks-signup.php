868ebe87d4671b5191fa54cc729ef676802a3605
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-05-31 19:37:29 +0530

    lp pricing page update

---
+++ l/staging/wp-content/themes/ws-landing/page-templates/tpl-thanks-signup.php
@@ -0,0 +1,12 @@
+<?php
+/**
+Template Name: Thanks - Signup
+*/
+get_header(); ?>
+<main class="main-content">
+  <div class="firstbox">
+    <h1 class="text_center white">Thank You - Your registration was successful.</h1>
+    <p class="para_big text_center white">As a next step, we've sent you an email to confirm your email address. Once confirmed, you will be good to login & use the system.</p>
+    </div>
+</main>
+<?php get_footer();?>
