a0572c5099894f0e7f57799ca28ef9c819e65c19
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-05-31 10:59:34 +0530

    added signup thank you template

--- staging/wp-content/themes/workstatus/page-templates/tpl-thankyou.php
+++ staging/wp-content/themes/workstatus/page-templates/tpl-thankyou.php
@@ -1,14 +1,21 @@
 <?php 
 /*
 Template Name: Thanks Template
-*/ 
+*/
 get_header();
 ?>
 <main class="site__content">
 <section class="background_blue thank_you">
     <div class="firstbox">
-        <h1 class="text_center white">THANK YOU!</h1>
-        <p class="para_big text_center white">Thanks for your query. Our representative will get in touch with you soon</p>
+    <?php 
+    if( is_page(3407) ){
+    echo '<h1 class="text_center white">THANK YOU!</h1>
+    <p class="para_big text_center white">Thanks for your query. Our representative will get in touch with you soon</p>';
+    }else{
+    echo '<h1 class="text_center white">Thank You - Your registration was successful.</h1>
+    <p class="para_big text_center white">As a next step, we\'ve sent you an email to confirm your email address. Once confirmed, you will be good to login & use the system.</p>';    
+    }
+    ?>        
     </div>
     <div class="secondbox">
         <a href="https://www.workstatus.io/" class="white_btn thank_youbtn">Go Back Home</a>
