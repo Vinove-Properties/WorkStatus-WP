1d894a1cef703cd50a9f4c41ca8b5c6c7de82941
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-06-20 10:54:00 +0530

    PPC page lead form - added

--- l/staging/wp-content/themes/ws-landing/inc/cmn-leadform.php
+++ l/staging/wp-content/themes/ws-landing/inc/cmn-leadform.php
@@ -11,64 +11,17 @@
         <form>
           <div class="signwrap d-flex">
             <div class="form-group">
-            <input type="text"  class="" placeholder="Full Name">
+              <input type="text" class="" placeholder="Full Name">
             </div>
             <div class="form-group">
               <input type="email" name="ws-email" id="sgemail1" class="emailfld" placeholder="Email">
             </div>
-            <div class="form-group sg-pcode" id="sg-wrap">
-              <div class="user-input">
-                <div class="iti iti--allow-dropdown">
-                  <div class="iti__flag-container">
-                    <div class="iti__selected-flag" role="combobox" aria-controls="iti-1__country-listbox" aria-owns="iti-1__country-listbox" aria-expanded="false" tabindex="0" title="India (भारत): +91" aria-activedescendant="iti-1__item-in">
-                      <div class="iti__flag iti__in"></div>
-                      <div class="iti__arrow"></div>
-                    </div>
-                    <ul class="iti__country-list iti__hide" id="iti-1__country-listbox" role="listbox" aria-label="List of countries">
-                      <li class="iti__country iti__preferred" tabindex="-1" id="iti-1__item-us-preferred" role="option" data-dial-code="1" data-country-code="us" aria-selected="false">
-                        <div class="flg-container">
-                          <div class="iti__flag-box">
-                            <div class="iti__flag iti__us"></div>
-                          </div>
-                          <span class="iti__country-name">United States</span><span class="iti__dial-code">+1</span>
-                          <div></div>
-                        </div>
-                      </li>
-                      <li class="iti__country iti__preferred" tabindex="-1" id="iti-1__item-gb-preferred" role="option" data-dial-code="44" data-country-code="gb" aria-selected="false">
-                        <div class="flg-container">
-                          <div class="iti__flag-box">
-                            <div class="iti__flag iti__gb"></div>
-                          </div>
-                          <span class="iti__country-name">United Kingdom</span><span class="iti__dial-code">+44</span>
-                          <div></div>
-                        </div>
-                      </li>
-                      <li class="iti__divider" role="separator" aria-disabled="true"></li>
-                      <li class="iti__country iti__standard" tabindex="-1" id="iti-1__item-af" role="option" data-dial-code="93" data-country-code="af" aria-selected="false">
-                        <div class="flg-container">
-                          <div class="iti__flag-box">
-                            <div class="iti__flag iti__af"></div>
-                          </div>
-                          <span class="iti__country-name">Afghanistan (&#x202B;افغانستان&#x202C;&lrm;)</span><span class="iti__dial-code">+93</span>
-                          <div></div>
-                        </div>
-                      </li>
-                      <li class="iti__country iti__standard" tabindex="-1" id="iti-1__item-al" role="option" data-dial-code="355" data-country-code="al" aria-selected="false">
-                        <div class="flg-container">
-                          <div class="iti__flag-box">
-                            <div class="iti__flag iti__al"></div>
-                          </div>
-                          <span class="iti__country-name">Albania (Shqipëri)</span><span class="iti__dial-code">+355</span>
-                          <div></div>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                  <input id="sg-pcode" type="tel" name="cprefix" maxlength="5" class="pcode-prefix" onkeydown="javascript:if(event.keyCode != 9) return false;" tabindex="-1" onmousedown="return false;" autocomplete="off" data-intl-tel-input-id="1">
-                </div>
-              </div>
-              <input id="sg-contphpne" type="tel" name="phone" maxlength="12" class="input-field phone-field" placeholder="Phone Number*" data-errelm="phone-errorelm">
-              <span class="error" id="phone-errorelm"></span>
+            <div class="form-group">
+            <div class="user-input">
+            <input id="pcode" type="tel" name="cprefix" maxlength="5" class="pcode-prefix" onkeydown="javascript:if(event.keyCode != 9) return false;" tabindex="-1" onmousedown="return false;">
+            <input id="cont_phpne" type="tel" name="phone" maxlength="12" class="input-field" placeholder="Phone Number*">
+            <span class="errors" id="phone-error"></span>
+            </div>
             </div>
             <div class="form-group">
               <textarea class="textarea-input" placeholder="Project Brief" name="requirement"></textarea>
