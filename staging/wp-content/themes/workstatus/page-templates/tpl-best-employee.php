ded8da3662558143ccd5527945568065aef08447
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-07-31 17:10:20 +0530

    update banner reviews

--- staging/wp-content/themes/workstatus/page-templates/tpl-best-employee.php
+++ staging/wp-content/themes/workstatus/page-templates/tpl-best-employee.php
@@ -40,7 +40,54 @@ if( $isEnable == "yes" ){
               </div>
 
               <?php require_once get_template_directory().'/common/available-device.php'; ?>
-              <?php require_once get_template_directory(). '/common/partners.php'; ?>
+              <?php //require_once get_template_directory(). '/common/partners.php'; ?>              
+              <div class="banner-award">
+              <div class="award-box">
+                <div class="client-section">
+                  <div class="client-row">
+                    <div class="client-stack award-animate-slide-to-left hover:pause">
+                      <ul>
+                        <li>
+                          <div class="star-rt">4.6 Excellent <img loading="lazy" src="<?php bloginfo('template_url'); ?>/assests/images/4.6star.svg" alt="Award" width="100" height="17"></div>
+                          <p>Invoicera is a lifesaver! I always know who's paid and who owes me. It keeps my finances organized, saving me time and stress, especially at tax time.</p>
+                          <picture class="awimg">
+                            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/assests/images/bnr-aw01.svg" alt="Capterra" width="130" height="29">
+                          </picture>
+                        </li>
+                        <li>
+                          <div class="star-rt">4.5 Excellent <img loading="lazy" src="<?php bloginfo('template_url'); ?>/assests/images/4.5star.svg" alt="Award" width="100" height="17"></div>
+                          <p>Invoicera makes online billing incredibly easy. I can send unlimited invoices quickly and without any hassle. It's a fantastic tool that has simplified my invoicing process.</p>
+                          <picture class="awimg">
+                            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/assests/images/bnr-aw02.svg" alt="G2" width="130" height="29">
+                          </picture>
+                        </li>
+                        <li>
+                          <div class="star-rt">4.6 Excellent <img loading="lazy" src="<?php bloginfo('template_url'); ?>/assests/images/4.6star.svg" alt="Award" width="100" height="17"></div>
+                          <p>I appreciate Invoicera's customization options, especially for branding my invoices. Granular permissions give me total peace of mind.</p>
+                          <picture class="awimg">
+                            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/assests/images/bnr-aw03.svg" alt="Software Advice" width="130" height="29">
+                          </picture>
+                        </li>
+                        <li>
+                          <div class="star-rt">4.6 Excellent <img loading="lazy" src="<?php bloginfo('template_url'); ?>/assests/images/4.6star.svg" alt="Award" width="100" height="17"></div>
+                          <p>Invoicera has boosted my professionalism and saved me tons of time! I love the easy invoice customization and clear history.</p>
+                          <picture class="awimg">
+                            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/dev-img/get-app.svg" alt="GetApp" width="108" height="20">
+                          </picture>
+                        </li>
+                        <li>
+                          <div class="star-rt">4.6 Excellent <img loading="lazy" src="<?php bloginfo('template_url'); ?>/assests/images/4.5star.svg" alt="Award" width="100" height="17"></div>
+                          <p>I've been using Invoicera for over 3 years to manage my invoices. It's incredibly easy to use and gives total control over my cash flow.</p>
+                          <picture class="awimg">
+                            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/dev-img/goodfirms.svg" alt="GoodFirms" width="119" height="18">
+                          </picture>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
               <?php 
               $vdScreenshot = '';
               if( isset( $top_section['video_background_image'] ) && !empty( $top_section['video_background_image'] ) 
