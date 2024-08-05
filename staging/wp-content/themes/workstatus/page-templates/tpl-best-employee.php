88d6c79fbbd0e7c9e7e584849e9c1435e7ece8ea
nandani <nandani.sinha@mail.vinove.com>
2024-08-05 13:36:40 +0530

    DATA Section added in best emp template.

--- staging/wp-content/themes/workstatus/page-templates/tpl-best-employee.php
+++ staging/wp-content/themes/workstatus/page-templates/tpl-best-employee.php
@@ -206,6 +206,58 @@ if( $isEnable == "yes" ){
 
 		  <?php } endif; ?>
 
+
+
+      <section class="data-driving">
+   <div class="container">
+      <div class="top-section">
+         <h6><span class="bg-purple">Data Driving Results</span></h6>
+         <h2>Smart Decisions for Business Growth: Plan, Analyze, and Execute</h2>
+         <p>Stay one step ahead always, manage & track your workforce & workload, analyze data and ensure business growth.</p>
+      </div>
+      <div class="data-flex">
+         <div class="column challenges">
+            <div class="head-data">
+               <div class="step-number">1</div>
+               <h3>Challenges</h3>
+            </div>
+            <ul>
+               <li>Inefficient Time Tracking</li>
+               <li>Real-Time Project / Task Monitoring</li>
+               <li>Difficulties in Managing Remote Teams</li>
+               <li>Poor Resource Allocation Impacting Outcomes</li>
+            </ul>
+         </div>
+         <div class="column analyze">
+            <div class="head-data">
+               <div class="step-number">2</div>
+               <h3>Analyze</h3>
+            </div>
+            <div class="analyze-button">
+               <div class="play-icon"> <img loading="lazy" src="<?php bloginfo('template_url'); ?>/assests/images/data-circle2.svg" alt="play" width="270" height="270">
+               </div>
+               <div class="playsc" onclick="return callinVideo();">
+                  <span class="playicon">Play</span>
+               </div>
+               <p>Play to watch</p>
+            </div>
+         </div>
+         <div class="column solutions">
+            <div class="head-data">
+               <div class="step-number">3</div>
+               <h3>Solutions</h3>
+            </div>
+            <ul>
+               <li>Automated, accurate timekeeping reduces manual errors</li>
+               <li>Smart dashboards provide instant project and productivity insights</li>
+               <li>Tools track and analyze remote employee activities for better management</li>
+               <li>Optimize workforce deployment based on data-driven insights</li>
+            </ul>
+         </div>
+      </div>
+   </div>
+</section>
+
                 
       <?php  
 		$boost_profitability = get_field('boost_profitability');
