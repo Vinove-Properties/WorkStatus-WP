73a4124d033a8567b9b56cc46a00de9d5acfbb09
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-08-05 16:28:53 +0530

    feature page section update

--- staging/wp-content/themes/workstatus/inc/template-functions.php
+++ staging/wp-content/themes/workstatus/inc/template-functions.php
@@ -284,8 +284,10 @@ function getResultsDriveSection( $post_id ){
                 alt="play" width="270" height="270">
                </div>
                <div class="playsc" onclick="return _playYTmedia(\''.$elm['yt-video'].'\');">
-               <span class="playicon">Play</span></div>
-               <p>Play to watch</p>
+                 <span class="playicon">Play</span>
+                 <p>Play to watch</p>
+               </div>
+               
             </div>
          </div>
          <div class="column solutions">
