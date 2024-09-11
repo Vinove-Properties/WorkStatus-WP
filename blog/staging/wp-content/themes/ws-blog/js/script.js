831cacb0c26707217fdf4c69c0cecd1e15195107
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-09-11 15:13:32 +0530

    utm param fixes on blog

--- blog/staging/wp-content/themes/ws-blog/js/script.js
+++ blog/staging/wp-content/themes/ws-blog/js/script.js
@@ -408,3 +408,61 @@ function gblClosePopup(elm){
     elmClose.style.display = "none";
   }
 }
+
+
+/*UTM Tracking*/
+function utm_getQueryParams() {
+    const params = {};
+    const queryString = window.location.search.substring(1);
+    const queryArray = queryString.split("&");
+    queryArray.forEach(function(item) {
+        const [key, value] = item.split("=");
+        params[key] = decodeURIComponent(value);
+    });
+    return params;
+}
+
+function utm_setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+const params    = utm_getQueryParams();
+const utmParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term', 'r_id' ];
+const utmValues = {};
+
+utmParams.forEach(param => {
+if (params[param]) {
+    if( param === "r_id" ){
+    utm_setCookie("ws_reftoken", params[param], 1);    
+    }else{
+    utm_setCookie(param, params[param], 1);    
+    }
+    
+}
+});
+
+function utm_getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+utmParams.forEach(param => {
+    const value = utm_getCookie(param);
+    if (value) {
+        utmValues[param] = value;
+    }
+});
+utmValues['utm_term'] = document.body.getAttribute('data-url');
+function generateUtmLink(baseUrl, utmParams) {
+    const url = new URL(baseUrl);
+    Object.keys(utmParams).forEach(param => {
+        if( utmValues[utmParams[param]] ){
+            url.searchParams.append(utmParams[param], utmValues[utmParams[param]]);    
+        }        
+    });
+    return url.toString();
+}
