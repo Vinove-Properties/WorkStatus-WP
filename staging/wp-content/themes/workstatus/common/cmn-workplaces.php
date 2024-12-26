47b9bc56747319752cdfec58734b7713b7f07972
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-12-26 17:11:16 +0530

    update feture 2.0 template

--- staging/wp-content/themes/workstatus/common/cmn-workplaces.php
+++ staging/wp-content/themes/workstatus/common/cmn-workplaces.php
@@ -3,11 +3,13 @@ $elmClass = (isset($args['elm-class']) && !empty($args['elm-class'])) ? $args['e
 ?>
 <section class="<?php echo $elmClass; ?>">
 <div class="container">
+
 <div class="top-section text-center">
 <h6><span class="bg-purple">For All Workplaces</span></h6>
 <h2>Collaborate Seamlessly Wherever You Work</h2>
 <p>Work together effortlessly, no matter where your team is located, with tools that keep communication and collaboration smooth.</p>
 </div>
+
 <div class="workplace-row justify-sb dis-flex">
   <div class="workplace-card">
     <div class="flip-card-inner">
@@ -19,12 +21,10 @@ $elmClass = (isset($args['elm-class']) && !empty($args['elm-class'])) ? $args['e
             <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/workplace-01.png" 
             alt="Workstatus" width="44" height="44">
           </picture>
-          <h3>Remote Work</h3>
-          <p>Stay connected and accountable</p>
+          <h3>Remote Work</h3><p>Stay connected and accountable</p>
         </div>
         <div class="card-bottom">
-          <h4>Achieve <strong>30%</strong></h4>
-          <span class="prod">Higher Accountability</span>
+          <h4>Achieve <strong>30%</strong></h4><span class="prod">Higher Accountability</span>
         </div>
       </div>
       <div class="flip-card-back">
@@ -48,9 +48,7 @@ $elmClass = (isset($args['elm-class']) && !empty($args['elm-class'])) ? $args['e
             <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/workplace-02.png">
             <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/workplace-02.png" alt="Workstatus" width="44" height="44">
           </picture>
-          <h3>Hybrid Work</h3>
-          <p>Balance productivity everywhere
-          </p>
+          <h3>Hybrid Work</h3><p>Balance productivity everywhere</p>
         </div>
         <div class="card-bottom">
           <h4>Get <strong>40%</strong></h4>
@@ -78,19 +76,16 @@ $elmClass = (isset($args['elm-class']) && !empty($args['elm-class'])) ? $args['e
             <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/workplace-03.png">
             <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/workplace-03.png" alt="Workstatus" width="44" height="44">
           </picture>
-          <h3>Field Work</h3>
-          <p>Empower field team efficiency</p>
+          <h3>Field Work</h3><p>Empower field team efficiency</p>
         </div>
         <div class="card-bottom">
-          <h4>Experience <strong>35%</strong></h4>
-          <span class="prod">Task Efficiency</span>
+          <h4>Experience <strong>35%</strong></h4><span class="prod">Task Efficiency</span>
         </div>
       </div>
       <div class="flip-card-back">
         <h3>Field Work</h3>
         <ul>
-          <li>Keep field teams connected and productive
-          </li>
+          <li>Keep field teams connected and productive</li>
           <li>Streamline communication and optimize field operations</li>
         </ul>
         <div class="card-bottom">
@@ -108,19 +103,16 @@ $elmClass = (isset($args['elm-class']) && !empty($args['elm-class'])) ? $args['e
             <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/workplace-04.png">
             <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/workplace-04.png" alt="Workstatus" width="44" height="44">
           </picture>
-          <h3>Office Work</h3>
-          <p>Optimize office productivity</p>
+          <h3>Office Work</h3><p>Optimize office productivity</p>
         </div>
         <div class="card-bottom">
-          <h4>See <strong>35%</strong></h4>
-          <span class="prod">Enhanced Productivity</span>
+          <h4>See <strong>35%</strong></h4><span class="prod">Enhanced Productivity</span>
         </div>
       </div>
       <div class="flip-card-back">
         <h3>Office Work</h3>
         <ul>
-          <li>Refine office operations by tracking employee attendance
-          </li>
+          <li>Refine office operations by tracking employee attendance</li>
           <li>Get real-time insights into productivity</li>
         </ul>
         <div class="card-bottom">
