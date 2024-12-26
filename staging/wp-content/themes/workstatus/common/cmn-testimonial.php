a65c33d577434ec43db116a5079f2985098ab7cd
nandani <nandani.sinha@mail.vinove.com>
2024-12-26 16:54:27 +0530

    Test Changes

--- staging/wp-content/themes/workstatus/common/cmn-testimonial.php
+++ staging/wp-content/themes/workstatus/common/cmn-testimonial.php
@@ -11,9 +11,9 @@
         <div class="inner-box">
           <p> “Workstatus simplified our workforce management and HR operations. Workstatus allowed us to track attendance seamlessly and accurately. Employees can clock in and clock out with a simple tap on their smartphones. We can monitor attendance in real-time, generate attendance &amp; productivity reports, and manage leave requests. I highly recommend Workstatus to any business that wants to automate their attendance tracking and improve HR operations”</p>
           <picture>
-            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png"
+            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Rupple-Nagpal.png">
+            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Rupple-Nagpal.png">
+            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Rupple-Nagpal.png"
               alt="Workstatus" width="77" height="77">
           </picture>
           <h5>Ripple Nagpal</h5>
@@ -24,9 +24,9 @@
         <div class="inner-box">
           <p> “I have been using Workstatus time tracking software for my team. I must say it has made a significant difference in my productivity and time management. The software is incredibly easy to use and it allows us to track our time accurately for each project and task. I highly recommend Workstatus time tracking software to any business looking to streamline their time management and maximize the productivity.”</p>
           <picture>
-            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png"
+            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Chetan-Pabari.png">
+            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Chetan-Pabari.png">
+            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Chetan-Pabari.png"
               alt="Workstatus" width="77" height="77">
           </picture>
           <h5>Chetan Pabari</h5>
@@ -37,9 +37,9 @@
         <div class="inner-box">
           <p>“We started using Workstatus sometime back. It has given a good amount of improvement in productivity of our employee. We are able to track what they do and how much time they spend on each of our project. It gives us clarity on which project we need to spend more time and whether they are spending right amount of time over the project as well. We are also able to track if anyone is tampering with productivity and time. As a productivity tracker, Workstatus is really good. It has a good support team that resolved all our technical issues in no time. We are looking for a long term association with Workstatus.”</p>
           <picture>
-            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png"
+            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Karthik.png">
+            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Karthik.png">
+            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Karthik.png"
               alt="Workstatus" width="77" height="77">
           </picture>
           <h5>Karthik</h5>
@@ -50,9 +50,9 @@
         <div class="inner-box">
           <p>“So far, I’m very happy with the support. It’s been very easy to set up the application. We contact via chats and WhatsApp. It has been a month, and it has been great. The support is awesome. There’ve been some tweaks here &amp; there, and a learning curve. I do recommend this application for your company.”</p>
           <picture>
-            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png"
+            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Alvora.png">
+            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Alvora.png">
+            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Alvora.png"
               alt="Workstatus" width="77" height="77">
           </picture>
           <h5>Alvaro J Ruiz</h5>
@@ -63,9 +63,9 @@
         <div class="inner-box">
           <p>“I think Workstatus is a very good application which I would recommend. It just doesn’t track time but helps us understand how team is performing. It always boosts team morale and keeps up good performance. I would recommend Workstatus for any organization for time tracking.”</p>
           <picture>
-            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png"
+            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Sudha.png">
+            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Sudha.png">
+            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Sudha.png"
               alt="Workstatus" width="77" height="77">
           </picture>
           <h5>Sudha</h5>
@@ -76,9 +76,9 @@
         <div class="inner-box">
           <p>“Time management is a crucial thing for us. Workstatus has made it easier to focus on what matters the most. It helps in keeping away distractions and focus more on projects so that we are able to deliver the best outcomes to users.”</p>
           <picture>
-            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png">
-            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/test-slide01.png"
+            <source type="image/webp" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Akash.png">
+            <source type="image/png" srcset="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Akash.png">
+            <img loading="lazy" src="<?php bloginfo('template_url'); ?>/version-2.0/assests/images/Akash.png"
               alt="Workstatus" width="77" height="77">
           </picture>
           <h5>Akash</h5>
