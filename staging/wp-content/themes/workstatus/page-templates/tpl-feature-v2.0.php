e02d4fb2d6c68b4d059f51e1e5f4f85e58b8ac7b
Nitin Baluni <nitin.baluni@mail.vinove.com>
2024-12-30 10:19:05 +0530

    feature template update

--- staging/wp-content/themes/workstatus/page-templates/tpl-feature-v2.0.php
+++ staging/wp-content/themes/workstatus/page-templates/tpl-feature-v2.0.php
@@ -453,7 +453,7 @@ if( isset( $faq['is_enabled'] ) && ($faq['is_enabled'] == "yes") ) :
 <?php //get_template_part('common/home', 'testimonials'); ?>
 <?php //require_once get_template_directory() .'/common/workstatus-journey.php';?>
 
-<?php get_template_part('common', 'blog'); ?>
+<?php get_template_part('common/blog'); ?>
 <?php get_template_part('common/cmn', 'testimonial'); ?>
 <?php get_template_part('common/cmn', 'globalrating'); ?>
 <?php get_template_part('common/cmn', 'journey'); ?>
