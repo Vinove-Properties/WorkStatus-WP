6e2b3e1961726118a5faeaaa1bf39ab91e4ae8cf
Nitin Baluni <nitin.baluni@mail.vinove.com>
2025-01-10 15:00:36 +0530

    slider loading issue fixes

--- staging/wp-content/themes/workstatus/page-templates/tpl-home-v4.0.php
+++ staging/wp-content/themes/workstatus/page-templates/tpl-home-v4.0.php
@@ -361,7 +361,7 @@
       <?php if($featureSlide['cards']) : ?>
       <div class="dis-flex featured-out">
         <div class="dis-flex featured-slider" id="featured-slide">
-          <div id="ft-slide-loader" class="loader">Loading...</div>
+          <div id="ft-glider-loader" class="loader">Loading...</div>
           <div class="glider" id="ft-glider">
             <?php 
               foreach($featureSlide['cards'] as $row){
