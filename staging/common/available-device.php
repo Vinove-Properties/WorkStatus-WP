dc01ef25b880c2a226a779013d0465eec504b5f9
Nitin Baluni <nitin.baluni@mail.vinove.com>
2025-03-20 13:26:42 +0530

    update featire page template

--- staging/common/available-device.php
+++
@@ -1,7 +0,0 @@
-<div class="av-device"><span>Available for:</span>
-  <i class="icon1"></i>
-  <i class="icon2"></i>
-  <i class="icon3"></i>
-  <i class="icon4"></i>
-  <i class="icon5"></i>
-</div>
