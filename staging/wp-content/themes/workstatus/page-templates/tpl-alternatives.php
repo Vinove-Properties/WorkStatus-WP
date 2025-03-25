6be472bd6010144192b22d838104a6e38138470f
nandani <nandani.sinha@mail.vinove.com>
2025-03-25 13:58:56 +0530

    WS Changes

--- staging/wp-content/themes/workstatus/page-templates/tpl-alternatives.php
+++ staging/wp-content/themes/workstatus/page-templates/tpl-alternatives.php
@@ -287,8 +287,8 @@ if( isset($faq['is_enabled']) && ($faq['is_enabled'] == "yes") ) :
    </div>
 </section>
 <?php endif; ?>
-<?php //get_template_part('common/home', 'testimonials', ['bg' => 'none']); ?>
-<?php //require_once get_template_directory() .'/common/all-brands.php';?>
-<?php //require_once get_template_directory() .'/common/workstatus-journey-straight.php';?>
+<?php get_template_part('common/cmn', 'testimonial'); ?>
+  <?php get_template_part('common/cmn', 'globalrating'); ?>
+  <?php get_template_part('common/cmn', 'journey'); ?>
 </main>
 <?php get_footer(); ?>
