145cad883ec8a8c56536a46ceec2656eaa5608ac
nandani <nandani.sinha@mail.vinove.com>
2025-05-22 16:06:10 +0530

    All pages common pricing added

--- staging/wp-content/themes/workstatus/common/pricing-v5.0.php
+++ staging/wp-content/themes/workstatus/common/pricing-v5.0.php
@@ -7,11 +7,11 @@ $activePlan =  'productivity';
 }
 ?>
 <div id="wsio-preloader" style="margin-top:100px;"><div class="container" id="pre-loader">Please wait, while we load the pricing plans for you.</div></div>
-<div id="wsio-pricing" class="pricing-wrapper" style="display:none;">
-  <section class="hero-banner no-af-bg post-animation">
+<div id="wsio-pricing" class="pricing-wrapper padding-t-120 bg-light padding-b-120" style="display:none;">
+  <section class="cmn-pricing no-af-bg post-animation">
     <div class="container">
       <div class="inner-wrap">
-        <div class="left">
+        <div class="left" style="text-align:center;">
           <h1>Simple, Flexible Pricing for Every Business</h1>
           <p><span class="no-hidden">No hidden fees</span>—just simple, affordable pricing for teams of all sizes.</p>
           <!-- <p> Can't find what you are looking for?  <span class="conbtn" onclick="getCalendlyiFrame();"><span>Setup a consulting call</span></span></p>-->
