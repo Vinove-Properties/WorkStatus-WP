5247605b8915dd94d4d5ce7e280c923b2f3e7c11
nandani <nandani.sinha@mail.vinove.com>
2025-05-28 13:34:03 +0530

    WS Changes

--- staging/web-html/version-2.0/common/footer.php
+++ staging/web-html/version-2.0/common/footer.php
@@ -306,7 +306,7 @@
 
 
       <div class="flex-3 copyright">
-        <a href="//www.dmca.com/Protection/Status.aspx?ID=9f4af2d1-a5c5-4031-903c-b6dfb2c56625" title="DMCA.com Protection Status" style="margin-left:0;margin-top: 20px; display:block" class="dmca-badge">
+        <a href="//www.dmca.com/Protection/Status.aspx?ID=9f4af2d1-a5c5-4031-903c-b6dfb2c56625" title="DMCA.com Protection Status" style="margin-left:0; display:block" class="dmca-badge">
         <img src ="https://images.dmca.com/Badges/dmca-badge-w200-5x1-06.png?ID=9f4af2d1-a5c5-4031-903c-b6dfb2c56625"  alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
       </div>
     </div>
